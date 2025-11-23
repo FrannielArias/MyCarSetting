228a828643d5743aecf6fb781d47e20d5af284cb
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2025-11-23 14:42:34 -0400

    Modificando el tema

--- app/src/main/java/edu/ucne/loginapi/presentation/MyCarSettingApp.kt
+++ app/src/main/java/edu/ucne/loginapi/presentation/MyCarSettingApp.kt
@@ -1,15 +1,16 @@
 package edu.ucne.loginapi.presentation
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import edu.ucne.loginapi.ui.theme.LoginApiTheme
+
 
 @Composable
 fun MyCarSettingApp() {
-    LoginApiTheme {
+    MaterialTheme {
         val navController = rememberNavController()
 
         Scaffold(
--- app/src/main/java/edu/ucne/loginapi/ui/theme/Theme.kt
+++ app/src/main/java/edu/ucne/loginapi/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.compose
+package edu.ucne.loginapi.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -255,25 +255,26 @@ val unspecified_scheme = ColorFamily(
 )
 
 @Composable
-fun AppTheme(
+fun LoginApiTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable() () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+  val colorScheme = when {
+      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+          val context = LocalContext.current
+          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+      }
+      
+      darkTheme -> darkScheme
+      else -> lightScheme
+  }
 
-        darkTheme -> darkScheme
-        else -> lightScheme
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography(),
-        content = content
-    )
+  MaterialTheme(
+    colorScheme = colorScheme,
+    typography = AppTypography,
+    content = content
+  )
 }
+
