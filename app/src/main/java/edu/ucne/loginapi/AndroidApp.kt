57e59f6eb269f002ff25fe429881c177f0c1f2f8
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2025-11-24 22:31:25 -0400

    temas

--- app/src/main/java/edu/ucne/loginapi/AndroidApp.kt
+++ app/src/main/java/edu/ucne/loginapi/AndroidApp.kt
@@ -10,10 +10,11 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import edu.ucne.loginapi.presentation.MyCarSettingBottomBar
 import edu.ucne.loginapi.presentation.MyCarSettingNavHost
+import edu.ucne.loginapi.ui.theme.LoginApiTheme
 
 @Composable
 fun AndroidApp() {
-    MaterialTheme {
+    LoginApiTheme {
         val navController = rememberNavController()
         Surface(
             modifier = Modifier.fillMaxSize(),
