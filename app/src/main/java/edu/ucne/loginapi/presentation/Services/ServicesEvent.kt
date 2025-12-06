aadee4017a704cdb186f92610673bd83bf4760aa
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2025-12-06 16:44:55 -0400

    Actualizando los eventos para el filtrado de los servicios sercanos

--- app/src/main/java/edu/ucne/loginapi/presentation/Services/ServicesEvent.kt
+++ app/src/main/java/edu/ucne/loginapi/presentation/Services/ServicesEvent.kt
@@ -4,6 +4,7 @@ import ServiceCategory
 
 sealed interface ServicesEvent {
     data object LoadInitialData : ServicesEvent
+    data class LoadForLocation(val lat: Double, val lon: Double) : ServicesEvent
     data class OnCategorySelected(val category: ServiceCategory?) : ServicesEvent
     data class OnServiceClicked(val id: String) : ServicesEvent
     data object OnUserMessageShown : ServicesEvent
